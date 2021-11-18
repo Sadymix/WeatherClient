d84d6ebacbaaa5bfc7f5e649508ac485ebaaf569
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-18 13:28:58 +0100

    implemented RestTemplate bean

---
+++ src/main/java/configuration/BeanConfiguration.java
@@ -0,0 +1,24 @@
+package configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+@RequiredArgsConstructor
+public class BeanConfiguration {
+
+    private final RestTemplateBuilder builder;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return builder.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
