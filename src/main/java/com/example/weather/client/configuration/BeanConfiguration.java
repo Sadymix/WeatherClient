1af6d50120b656deeff66add3761f75b62efee2a
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-18 16:07:56 +0100

    implemented setNamingStrategy method to configure ObjectMapper and modified restTemplate bean

--- src/main/java/com/example/weather/client/configuration/BeanConfiguration.java
+++ src/main/java/com/example/weather/client/configuration/BeanConfiguration.java
@@ -1,6 +1,9 @@
-package configuration;
+package com.example.weather.client.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
@@ -13,12 +16,17 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class BeanConfiguration {
 
-    private final RestTemplateBuilder builder;
-
     @Bean
     public RestTemplate restTemplate() {
+        RestTemplateBuilder builder = new RestTemplateBuilder();
         return builder.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
+
+    @Autowired
+    public void setNamingStrategy(ObjectMapper objectMapper){
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+
 }
