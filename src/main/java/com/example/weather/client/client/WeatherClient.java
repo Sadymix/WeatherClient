571cce7bd06bf38b7a8b22723f220e26d30729fc
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-23 12:34:16 +0100

    removed post construct modified return type

--- src/main/java/com/example/weather/client/client/WeatherClient.java
+++ src/main/java/com/example/weather/client/client/WeatherClient.java
@@ -1,16 +1,11 @@
 package com.example.weather.client.client;
 
-import com.example.weather.client.mappers.WeatherDataMapper;
 import com.example.weather.client.models.dto.WeatherDataDto;
-import com.example.weather.client.models.entity.WeatherData;
-import com.example.weather.client.repositories.WeatherDataRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
 
 @Slf4j
 @Component
@@ -18,22 +13,13 @@ import javax.annotation.PostConstruct;
 public class WeatherClient {
 
     private final RestTemplate restTemplate;
-    private final WeatherDataRepo weatherDataRepo;
-    private final WeatherDataMapper weatherDataMapper;
     @Value("${app.url.base}")
     private String url;
     @Value("${openweather.api.key}")
     private String keyValue;
 
-    public WeatherData getWeather(String city) {
-        var forObject = restTemplate.getForObject(url + "?q=" + city + "&appid=" + keyValue,
+    public WeatherDataDto getWeather(String city) {
+        return restTemplate.getForObject(url + "?q=" + city + "&appid=" + keyValue,
                 WeatherDataDto.class);
-        var save = weatherDataRepo.save(weatherDataMapper.toEntity(forObject));
-        return save;
-    }
-
-    @PostConstruct
-    public void postConstruct() {
-        log.info(getWeather("Warsaw").toString());
     }
 }
