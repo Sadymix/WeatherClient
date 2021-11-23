8d4c3b3e2df0b0b7a5697acf3981b79757cbb785
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-23 14:19:59 +0100

    implemented mappers and test cases for their toEntity methods

--- src/test/java/com/example/weather/client/mappers/WeatherDataMapperTest.java
+++ src/test/java/com/example/weather/client/mappers/WeatherDataMapperTest.java
@@ -1,8 +1,9 @@
 package com.example.weather.client.mappers;
 
 import com.example.weather.client.models.dto.WeatherDataDto;
+import com.example.weather.client.models.dto.WeatherDto;
+import com.example.weather.client.models.entity.Weather;
 import com.example.weather.client.models.entity.WeatherData;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,13 +11,14 @@ import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @ExtendWith(MockitoExtension.class)
 class WeatherDataMapperTest {
 
     private static PodamFactory FACTORY = new PodamFactoryImpl();
     private static final WeatherDataDto WEATHER_DATA_DTO = FACTORY.manufacturePojo(WeatherDataDto.class);
-    private static WeatherData WEATHER_DATA;
 
     private GeoCoordinatesMapper geoCoordinatesMapper = new GeoCoordinatesMapper();
     private WeatherMapper weatherMapper = new WeatherMapper();
@@ -25,22 +27,28 @@ class WeatherDataMapperTest {
     private WeatherDataMapper weatherDataMapper =
             new WeatherDataMapper(geoCoordinatesMapper, weatherMapper, conditionsMapper, windMapper);
 
-
-    @BeforeEach
-    void setUp() {
-        WEATHER_DATA = new WeatherData()
-                .setCoordinates(geoCoordinatesMapper.toEntity(WEATHER_DATA_DTO.getCoord()))
-                .setWeathers(weatherMapper.toEntityList(WEATHER_DATA_DTO.getWeather()))
-                .setConditions(conditionsMapper.toEntity(WEATHER_DATA_DTO.getMain()))
-                .setWind(windMapper.toEntity(WEATHER_DATA_DTO.getWind()))
-                .setUnixTime(WEATHER_DATA_DTO.getUnixTime())
-                .setCityName(WEATHER_DATA_DTO.getName());
-    }
-
     @Test
     void testToEntity() {
         var weatherData = weatherDataMapper.toEntity(WEATHER_DATA_DTO);
         assertThat(weatherData).isNotNull();
-        assertThat(weatherData).isEqualTo(WEATHER_DATA);
+        assertEqualsForWeatherData(weatherData);
+    }
+
+    private void assertEqualsForWeatherData(WeatherData weatherData) {
+        assertEquals(weatherData.getCoordinates().getLongitude(), WEATHER_DATA_DTO.getCoord().getLon());
+        assertEquals(weatherData.getCoordinates().getLatitude(), WEATHER_DATA_DTO.getCoord().getLat());
+        assertEquals(weatherData.getWeathers().stream().map(Weather::getParameters).toList(),
+                WEATHER_DATA_DTO.getWeather().stream().map(WeatherDto::getMain).toList());
+        assertEquals(weatherData.getWeathers().stream().map(Weather::getDescription).toList(),
+                WEATHER_DATA_DTO.getWeather().stream().map(WeatherDto::getDescription).toList());
+        assertEquals(weatherData.getConditions().getTemperature(), WEATHER_DATA_DTO.getMain().getTemp());
+        assertEquals(weatherData.getConditions().getMaxTemperature(), WEATHER_DATA_DTO.getMain().getTempMax());
+        assertEquals(weatherData.getConditions().getMinTemperature(), WEATHER_DATA_DTO.getMain().getTempMin());
+        assertEquals(weatherData.getConditions().getPressure(), WEATHER_DATA_DTO.getMain().getPressure());
+        assertEquals(weatherData.getConditions().getHumidity(), WEATHER_DATA_DTO.getMain().getHumidity());
+        assertEquals(weatherData.getWind().getSpeed(), WEATHER_DATA_DTO.getWind().getSpeed());
+        assertEquals(weatherData.getWind().getDegrees(), WEATHER_DATA_DTO.getWind().getDeg());
+        assertEquals(weatherData.getUnixTime(), WEATHER_DATA_DTO.getUnixTime());
+        assertEquals(weatherData.getCityName(), WEATHER_DATA_DTO.getName());
     }
 }
---
+++ src/test/java/com/example/weather/client/mappers/WindMapperTest.java
@@ -0,0 +1,24 @@
+package com.example.weather.client.mappers;
+
+import com.example.weather.client.models.dto.WindDto;
+import org.junit.jupiter.api.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WindMapperTest {
+
+    private static PodamFactory FACTORY = new PodamFactoryImpl();
+    private static final WindDto WIND_DTO = FACTORY.manufacturePojo(WindDto.class);
+    private WindMapper windMapper = new WindMapper();
+
+    @Test
+    void toEntity() {
+        var wind = windMapper.toEntity(WIND_DTO);
+        assertThat(wind).isNotNull();
+        assertEquals(wind.getSpeed(), WIND_DTO.getSpeed());
+        assertEquals(wind.getDegrees(), WIND_DTO.getDeg());
+    }
+}
