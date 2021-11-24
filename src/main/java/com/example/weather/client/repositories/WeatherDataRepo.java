724c2ef2b83a4225c91e064f4dbbda7783c34652
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-24 14:43:04 +0100

    implemented method for finding weather data in time period

--- src/main/java/com/example/weather/client/repositories/WeatherDataRepo.java
+++ src/main/java/com/example/weather/client/repositories/WeatherDataRepo.java
@@ -3,5 +3,8 @@ package com.example.weather.client.repositories;
 import com.example.weather.client.models.entity.WeatherData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface WeatherDataRepo extends MongoRepository<WeatherData, String> {
+    List<WeatherData> findAllByUnixTimeGreaterThanAndUnixTimeLessThanEqual(Long fromTime, Long toTime);
 }
