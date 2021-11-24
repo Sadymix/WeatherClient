954722b3ff18ed94d93c07a1e25a6448f6034851
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-24 13:59:36 +0100

    implemented method findAllByCityName

--- src/main/java/com/example/weather/client/repositories/WeatherDataRepo.java
+++ src/main/java/com/example/weather/client/repositories/WeatherDataRepo.java
@@ -1,7 +1,10 @@
 package com.example.weather.client.repositories;
 
 import com.example.weather.client.models.entity.WeatherData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface WeatherDataRepo extends MongoRepository<WeatherData, String> {
+    Page<WeatherData> findAllByCityName(String city, Pageable pageable);
 }
