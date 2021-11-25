099e62a7033ae79aaf44dc4bbbc5f5275849bef2
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-25 09:48:20 +0100

    implemented new runtime exception

---
+++ src/main/java/com/example/weather/client/exceptions/MyResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.weather.client.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class MyResourceNotFoundException extends RuntimeException {
+
+    public MyResourceNotFoundException(String message) {
+        super(message);
+    }
+}
