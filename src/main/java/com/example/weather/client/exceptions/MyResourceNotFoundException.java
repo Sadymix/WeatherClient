2efd12910993b602f6e9ef383854b5cfeab56d08
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-25 10:39:27 +0100

    changed to change status to bad request(400) added message

--- src/main/java/com/example/weather/client/exceptions/MyResourceNotFoundException.java
+++ src/main/java/com/example/weather/client/exceptions/MyResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.weather.client.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "resource is to large")
 public class MyResourceNotFoundException extends RuntimeException {
 
     public MyResourceNotFoundException(String message) {
