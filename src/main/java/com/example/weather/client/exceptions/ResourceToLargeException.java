f5b89938699db5cdb9e6153ad8f3c5fb5d5b1b8d
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-25 10:56:28 +0100

    modified exception name

--- src/main/java/com/example/weather/client/exceptions/ResourceToLargeException.java
+++ src/main/java/com/example/weather/client/exceptions/ResourceToLargeException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "resource is to large")
-public class MyResourceNotFoundException extends RuntimeException {
+public class ResourceToLargeException extends RuntimeException {
 
-    public MyResourceNotFoundException(String message) {
+    public ResourceToLargeException(String message) {
         super(message);
     }
 }
