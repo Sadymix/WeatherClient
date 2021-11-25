28fd1136f2152b45a12cdd2e3ac0d71165bf9c8d
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-25 09:47:39 +0100

    implemented defaultValues for request parameters

--- src/main/java/com/example/weather/client/controller/WeatherDataController.java
+++ src/main/java/com/example/weather/client/controller/WeatherDataController.java
@@ -3,12 +3,11 @@ package com.example.weather.client.controller;
 import com.example.weather.client.models.dto.WeatherDataDto;
 import com.example.weather.client.services.WeatherDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +17,10 @@ public class WeatherDataController {
     private final WeatherDataService weatherDataService;
 
     @GetMapping
-    public List<WeatherDataDto> getWeatherDataByCity(
-            @RequestParam("city") String city,
-            @RequestParam("page") int page,
-            @RequestParam("size") int size) {
+    public Page<WeatherDataDto> getWeatherDataByCity(
+            @RequestParam(defaultValue = "Warsaw") String city,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
         return weatherDataService.getWeatherDataByCity(city, page, size);
     }
 }
