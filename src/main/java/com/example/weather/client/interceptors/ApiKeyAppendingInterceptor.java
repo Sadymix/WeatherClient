bd4583c5303eda3b73d8da01c022641779fb9171
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-30 15:31:19 +0100

    modified class name to ApiKeyAppendingInterceptor

--- src/main/java/com/example/weather/client/interceptors/ApiKeyAppendingInterceptor.java
+++ src/main/java/com/example/weather/client/interceptors/ApiKeyAppendingInterceptor.java
@@ -1,8 +1,7 @@
 package com.example.weather.client.interceptors;
 
-import com.example.weather.client.wrapper.HttpRequestWrapperImpl;
+import com.example.weather.client.wrapper.ApiKeyHttpRequestWrapper;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -10,9 +9,8 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
-public class AppendKeyToUrlInterceptor implements ClientHttpRequestInterceptor {
+public class ApiKeyAppendingInterceptor implements ClientHttpRequestInterceptor {
 
     @Value("${openweather.api.key}")
     private String keyValue;
@@ -20,6 +18,6 @@ public class AppendKeyToUrlInterceptor implements ClientHttpRequestInterceptor {
     @SneakyThrows
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) {
-        return execution.execute(new HttpRequestWrapperImpl(request, keyValue), body);
+        return execution.execute(new ApiKeyHttpRequestWrapper(request, keyValue), body);
     }
 }
