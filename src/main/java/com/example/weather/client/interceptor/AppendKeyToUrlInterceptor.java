f2809e08bd58865fddc9d38b2e562c091662a397
kkorzeniewski <kkorzeniewski@pgs-soft.com>
2021-11-30 13:03:06 +0100

    implemented @SneakyThrows annotation

--- src/main/java/com/example/weather/client/interceptor/AppendKeyToUrlInterceptor.java
+++ src/main/java/com/example/weather/client/interceptor/AppendKeyToUrlInterceptor.java
@@ -1,6 +1,7 @@
 package com.example.weather.client.interceptor;
 
 import com.example.weather.client.wrappers.HttpRequestWrapperImpl;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpRequest;
@@ -9,8 +10,6 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Slf4j
 @Component
 public class AppendKeyToUrlInterceptor implements ClientHttpRequestInterceptor {
@@ -18,10 +17,9 @@ public class AppendKeyToUrlInterceptor implements ClientHttpRequestInterceptor {
     @Value("${openweather.api.key}")
     private String keyValue;
 
+    @SneakyThrows
     @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        var uri = new HttpRequestWrapperImpl(request, keyValue).getURI();
-        log.info("overriding {}", uri);
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) {
         return execution.execute(new HttpRequestWrapperImpl(request, keyValue), body);
     }
 }
